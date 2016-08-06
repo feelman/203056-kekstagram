76d49a48a39d93ee2c8b25a782fb5afb94513907
feelman <feelman@list.ru>
2016-08-06 16:52:38 +0200

    Добавлена функция v.2

--- build/js/check.js
+++ build/js/check.js
@@ -2,15 +2,29 @@ function getMessage(a, b) {
   if (a == true) {
     return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
   }
+
   else if (a == false) {
     return 'Переданное GIF-изображение не анимировано';
   }
 
-  else if (a) {
+  else if (+a) {
     return 'Переданное SVG-изображение содержит ' + a + ' объектов ' + 'и ' + (b * 4) + ' атрибутов';
   }
 
-  else if (a == []) {
+  else if ([], b == undefined) {
+    var amountOfRedPoints = 0;
+    for (var i = 0; i < a.length; i++) {
+      amountOfRedPoints += a[i];
+    }
     return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
   }
+
+  else if ([],[]) {
+    var artifactsSquare = 0;
+    for (var i = 0; i < b.length; i++) {
+      artifactsSquare += a[i] * b[i];
+    }
+    return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+  }
+
 }
